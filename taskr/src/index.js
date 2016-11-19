0bf3aeb7d43c6ecd7e8fb88e772beca4dfc0099b
Christopher Bartling <chris.bartling@gmail.com>
2016-11-18 23:19:36 -0600

    Initial revision.

--- taskr/src/index.js
+++ taskr/src/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import firebase from "firebase";
+
+
+const config = {
+    apiKey: "AIzaSyB4kZD9hoMFucDLj9Oaz2XD6IguEUx3Owo",
+    authDomain: "taskr-demo.firebaseapp.com",
+    databaseURL: "https://taskr-demo.firebaseio.com",
+    storageBucket: "taskr-demo.appspot.com",
+    ymessagingSenderId: "1082586470711"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
