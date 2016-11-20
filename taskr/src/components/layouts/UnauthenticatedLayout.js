0d7e65036d0f5776b5572aa249b2a86bb77ffef6
Christopher Bartling <chris.bartling@gmail.com>
2016-11-19 18:46:52 -0600

    Checkpoint.

--- taskr/src/components/layouts/UnauthenticatedLayout.js
+++ taskr/src/components/layouts/UnauthenticatedLayout.js
@@ -1,3 +1,35 @@
-/**
- * Created by chris on 11/19/16.
- */
+// @flow
+
+import React, {Component} from "react";
+// import {Link} from "react-router";
+import "./UnauthenticatedLayout.css";
+
+export default class UnauthenticatedLayout extends Component {
+
+    render() {
+        return (
+            <div>
+                <section id="headerSection">
+                    <nav className="pt-navbar pt-dark">
+                        <div className="pt-navbar-group pt-align-left">
+                            <div className="pt-navbar-heading">Taskr</div>
+                        </div>
+                        <div className="pt-navbar-group pt-align-right">
+                            <button className="pt-button pt-minimal pt-icon-home">Home</button>
+                            <span className="pt-navbar-divider"></span>
+                            <button className="pt-button pt-minimal pt-icon-user"></button>
+                            <button className="pt-button pt-minimal pt-icon-notifications"></button>
+                            <button className="pt-button pt-minimal pt-icon-cog"></button>
+                        </div>
+                    </nav>
+                </section>
+                <section id="contentSection">
+                    {this.props.children}
+                </section>
+                <section id="footerSection">
+                    <div>Pintail Consulting LLC &copy; 2016</div>
+                </section>
+            </div>
+        );
+    }
+}
